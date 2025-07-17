c241df43ed45c4e0b8c6d46a26d0e688d8291d58
euyuriadriel <60609385+xCONEx@users.noreply.github.com>
2025-07-17 10:42:24 -0300

    Update proxy-clan.js

--- api/proxy-clan.js
+++ api/proxy-clan.js
@@ -1,11 +1,14 @@
 export default async function handler(req, res) {
-  const { playerName } = req.query;
-  if (!playerName) {
-    return res.status(400).json({ error: "playerName obrigatório" });
+  const { clanName } = req.query;
+  if (!clanName) {
+    return res.status(400).json({ error: "clanName obrigatório" });
   }
-  const url = `https://secure.runescape.com/m=hiscore/index_lite.ws?player=${encodeURIComponent(playerName)}`;
+  const url = `https://secure.runescape.com/m=clan-hiscores/members_lite.ws?clanName=${encodeURIComponent(clanName)}`;
   try {
     const response = await fetch(url);
+    if (!response.ok) {
+      return res.status(response.status).json({ error: "Erro ao buscar dados do RuneScape" });
+    }
     const text = await response.text(); // RuneScape retorna texto, não JSON
     res.setHeader('Content-Type', 'text/plain');
     res.status(200).send(text);
